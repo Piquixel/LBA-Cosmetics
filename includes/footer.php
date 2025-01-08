0efca4f89242f728ce820c403da3120838237c91
OliviaDemaret <oliviadtqb@gmail.com>
2025-01-08 21:18:20 +0100

    header and footer html

---
+++ includes/footer.php
@@ -0,0 +1,51 @@
+<footer>
+    <div>
+        <h2>Contact</h2>
+        <ul>
+            <li><p>Call Phone</p></li>
+            <li><i class="fa-solid fa-phone"></i><a href="#"> (+84) 123 567 712</a></li>
+            <li><p>Mail Us</p></li>
+            <li><i class="fa-regular fa-envelope"></i><a href="#"> Arioshop@Gmail.Com</a></li>
+        </ul>
+    </div>
+    <div>
+        <h2>About Us</h2>
+        <ul>
+            <li><a href="#">FAQs</a></li>
+            <li><a href="#">Shipping</a></li>
+            <li><a href="#">Returns</a></li>
+            <li><a href="#">Size Guides</a></li>
+            <li><a href="#">Materials & Care</a></li>
+            <li><a href="#">Contact Us</a></li>
+        </ul>
+    </div>
+    <div>
+        <h2>Corporate</h2>
+        <ul>
+            <li><a href="#">Careers</a></li>
+            <li><a href="#">About Us</a></li>
+            <li><a href="#">Code Of Ethics</a></li>
+            <li><a href="#">Event Sponsorships</a></li>
+            <li><a href="#">Vendors</a></li>
+            <li><a href="#">Affiliate Program</a></li>
+        </ul></div>
+    <div>
+        <h2>Follow Us</h2>
+        <ul>
+            <li><a href="#">Twitter</a></li>
+            <li><a href="#">Facebook</a></li>
+            <li><a href="#">Instagram</a></li>
+            <li><a href="#">Pinterest</a></li>
+        </ul>
+    </div>
+    <div>
+        <div>
+            <p>Subscribe to be aware of our regular promotions & exculives offers!</p>
+            <input>
+        </div>
+        <div>
+            <img>
+            <p>Copyright © 2024. All Rights Reserved.</p>
+        </div>
+    </div>
+</footer>
---
+++ includes/header.php
@@ -0,0 +1,49 @@
+<header>
+    <nav>
+        <div>
+        <div id="reseaux">
+            <ul>
+                <li>
+                    <a href="#"><i class="fa-brands fa-tiktok"></i></a></li>
+                <li>
+                    <a href="#"><i class="fa-brands fa-x-twitter"></i></a>
+                </li>
+                <li>
+                    <a href="#"><i class="fa-brands fa-instagram"></i></a>
+                </li>
+                <li>
+                    <a href="#"><i class="fa-brands fa-facebook-f"></i></a>
+                </li>
+            </ul>
+        </div>
+        <div>
+            <a href="./index.php"><img src="./images/logo.png"></a>
+        </div>
+        <div>
+            <i class="fa-solid fa-magnifying-glass"></i>
+            <i class="fa-regular fa-user"></i>
+            <i class="fa-sharp fa-regular fa-heart"></i>
+            <i class="fa-solid fa-bag-shopping"></i>
+        </div>
+</div>
+<div>
+    <ul>
+        <li>
+            <a href="#">Home</a>
+        </li>
+        <li>
+            <a href="#">Shop</a>
+        </li>
+        <li>
+            <a href="#">Products</a>
+        </li>
+        <li>
+            <a href="#">Blog</a>
+        </li>
+        <li>
+            <a href="#">Page</a>
+        </li>
+    </ul>
+</div>
+    </nav>
+</header>
