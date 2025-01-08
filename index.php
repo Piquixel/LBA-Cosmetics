760e73c45b9b97ce2cd3a846e9c70a99d42d0984
Piquixel <thomas.fetu2004@gmail.com>
2025-01-08 19:56:35 +0100

    added missing files + content

---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<meta http-equiv="X-UA-Compatible" content="IE=7" />
+	<meta http-equiv="X-UA-Compatible" content="ie=edge" />
+
+	<meta http-equiv="content-type" content="text/html" />
+
+	<title>Home 6 – Ario</title>
+
+	<link
+		rel="shortcut icon"
+		href="images/favicon/cropped-favicon-32x32.png"
+		type="image/png"
+		sizes="32x32" />
+	<link
+		rel="shortcut icon"
+		href="images/favicon/cropped-favicon-192x192.png"
+		type="image/png"
+		sizes="192x192" />
+	<link
+		rel="apple-touch-icon"
+		href="images/favicon/cropped-favicon-180x180.png"
+		type="image/png" />
+
+	<link rel="stylesheet" type="text/css" href="styles/main.css" />
+	<link rel="stylesheet" type="text/css" href="styles/animations.css" />
+	<link rel="stylesheet" type="text/css" href="styles/responsive.css" />
+
+	<script defer type="text/javascript" src="main.js"></script>
+</head>
+
+<body>
+	<!-- Gemini -->
+	<?php include 'includes/header.php' ?>
+	<main>
+		<!-- Ectho -->
+		<section id="slider"></section>
+		<!-- Ectho -->
+		<section class="carousel" id="top_rating_1"></section>
+		<!-- Gemini -->
+		<section id="to_shop"></section>
+		<!-- Gemini -->
+		<section id="help"></section>
+		<!-- Ectho -->
+		<section class="carousel" id="category_carousel"></section>
+		<!-- Gemini -->
+		<section id="product_showcase"></section>
+		<!-- Ectho -->
+		<section class="carousel" id="top_rating_2"></section>
+		<!-- Ectho -->
+		<section id="products_recommendation"></section>
+	</main>
+	<!-- Gemini -->
+	<?php include 'includes/footer.php' ?>
+</body>
+
+</html>
