0cb6bad96c04f95b9135d71229cad01c33e39e69
Piquixel <thomas.fetu2004@gmail.com>
2025-01-12 21:19:02 +0100

    Ectho's Sections

--- index.php
+++ index.php
@@ -41,21 +41,430 @@
 	<?php include 'includes/header.php' ?>
 	<main>
 		<!-- Ectho -->
-		<section id="slider"></section>
+		<section id="home_slider">
+			<button
+				id="slider_left_btn"
+				class="sliderInteractBtn fa-sharp fa-solid fa-angle-left"
+				style="left: 0"></button>
+			<div id="slider_container">
+				<div id="slide_1" class="sliderSlides">
+					<article>
+						<h1 class="slideTitle">
+							New <br />
+							explicit lipstick
+						</h1>
+						<span class="slideSubTitle">Makeup & Life blending</span>
+						<a href="#" class="shopBtn">Shop now</a>
+					</article>
+					<img
+						class="slideImg"
+						src="images/slider/slider-5-1.jpg"
+						alt="" />
+				</div>
+				<div id="slide_2" class="sliderSlides">
+					<article>
+						<h1 class="slideTitle">
+							soft <br />
+							genuine lipstick
+						</h1>
+						<span class="slideSubTitle">Your lips will be more perfect</span>
+						<a href="#" class="shopBtn">Shop now</a>
+					</article>
+					<img
+						class="slideImg"
+						src="images/slider/slider-5-2.jpg"
+						alt="" />
+				</div>
+				<div id="slide_3" class="sliderSlides">
+					<article>
+						<h1 class="slideTitle">
+							hot <br />
+							metallic glitter
+						</h1>
+						<span class="slideSubTitle">Good Eye catching effect</span>
+						<a href="#" class="shopBtn">shop now</a>
+					</article>
+					<img
+						class="slideImg"
+						src="images/slider/slider-5-3.jpg"
+						alt="" />
+				</div>
+			</div>
+			<button
+				id="slider_right_btn"
+				class="sliderInteractBtn fa-sharp fa-solid fa-angle-right"
+				style="right: 0"></button>
+		</section>
 		<!-- Ectho -->
-		<section class="carousel" id="top_rating_1"></section>
+		<section class="listSliderRow" id="top_rating_1">
+			<div class="titleContainer">
+				<h2>top rating</h2>
+				<span>Subscribe to be aware of our regular promotions</span>
+			</div>
+			<div class="contentList">
+				<button
+					class="sliderInteractBtn fa-sharp fa-solid fa-angle-left"
+					style="margin-left: -8px"></button>
+				<div class="listSlide">
+					<div class="slideEl">
+						<div
+							class="card"
+							style="
+									--thumb-var-1: url(../images/product/product-30-600x600.jpg);
+									--thumb-var-2: url(../images/product/product-29-600x600.jpg);
+								">
+							<div class="cardTags">
+								<span class="productTag tagHot">hot</span>
+								<span class="productTag tagSale">-17%</span>
+							</div>
+							<div class="cardInteract">
+								<button
+									class="fa-sharp fa-regular fa-heart"></button>
+								<button
+									class="fa-sharp fa-solid fa-magnifying-glass"></button>
+							</div>
+						</div>
+						<div class="cardDetails">
+							<h3><a href="#">the dream mask</a></h3>
+							<span>$50.00 <s>$60.00</s></span>
+						</div>
+					</div>
+					<div class="slideEl">
+						<div
+							class="card"
+							style="
+									--thumb-var-1: url(../images/product/product-48-600x600.jpg);
+									--thumb-var-2: url(../images/product/product-47-600x600.jpg);
+								">
+							<div class="cardInteract">
+								<button
+									class="fa-sharp fa-regular fa-heart"></button>
+								<button
+									class="fa-sharp fa-solid fa-magnifying-glass"></button>
+							</div>
+						</div>
+						<div class="cardDetails">
+							<h3><a href="#">The hydra serum</a></h3>
+							<span>$40.00</span>
+						</div>
+					</div>
+					<div class="slideEl">
+						<div
+							class="card"
+							style="
+									--thumb-var-1: url(../images/product/product-37-600x600.jpg);
+									--thumb-var-2: url(../images/product/product-38-600x600.jpg);
+								">
+							<div class="cardTags">
+								<span class="productTag tagHot">hot</span>
+							</div>
+							<div class="cardInteract">
+								<button
+									class="fa-sharp fa-regular fa-heart"></button>
+								<button
+									class="fa-sharp fa-solid fa-magnifying-glass"></button>
+							</div>
+						</div>
+						<div class="cardDetails">
+							<h3><a href="#">The face jelly</a></h3>
+							<span>$40.00 <s>$56.00</s></span>
+						</div>
+					</div>
+					<div class="slideEl">
+						<div
+							class="card"
+							style="
+									--thumb-var-1: url(../images/product/product-45-600x600.jpg);
+									--thumb-var-2: url(../images/product/product-46-600x600.jpg);
+								">
+							<div class="cardTags">
+								<span class="productTag tagSale">-15%</span>
+							</div>
+							<div class="cardInteract">
+								<button
+									class="fa-sharp fa-regular fa-heart"></button>
+								<button
+									class="fa-sharp fa-solid fa-magnifying-glass"></button>
+							</div>
+						</div>
+						<div class="cardDetails">
+							<h3><a href="#">the glow serum</a></h3>
+							<span>$55.00 <s>$65.00</s></span>
+						</div>
+					</div>
+					<div class="slideEl">
+						<div
+							class="card"
+							style="
+									--thumb-var-1: url(../images/product/360-1-600x600.jpg);
+									--thumb-var-2: url(../images/product/360-2-600x600.jpg);
+								">
+							<div class="cardTags">
+								<span class="productTag tagHot">hot</span>
+							</div>
+							<div class="cardInteract">
+								<button
+									class="fa-sharp fa-regular fa-heart"></button>
+								<button
+									class="fa-sharp fa-solid fa-magnifying-glass"></button>
+							</div>
+						</div>
+						<div class="cardDetails">
+							<h3>
+								<a href="#">daymask peptide lip balm</a>
+							</h3>
+							<span>$50.00</span>
+						</div>
+					</div>
+					<div class="slideEl">
+						<div
+							class="card"
+							style="
+									--thumb-var-1: url(../images/product/product-62-600x600.jpg);
+									--thumb-var-2: url(../images/product/product-63-600x600.jpg);
+								">
+							<div class="cardTags">
+								<span class="productTag tagSale">-5%</span>
+							</div>
+							<div class="cardInteract">
+								<button
+									class="fa-sharp fa-regular fa-heart"></button>
+								<button
+									class="fa-sharp fa-solid fa-magnifying-glass"></button>
+							</div>
+						</div>
+						<div class="cardDetails">
+							<h3><a href="#">the wow serum</a></h3>
+							<span>$95.00 <s>$100.00</s></span>
+						</div>
+					</div>
+				</div>
+				<button
+					class="sliderInteractBtn fa-sharp fa-solid fa-angle-right"
+					style="right: 8px"></button>
+			</div>
+		</section>
 		<!-- Gemini -->
 		<section id="to_shop"></section>
 		<!-- Gemini -->
 		<section id="help"></section>
 		<!-- Ectho -->
-		<section class="carousel" id="category_carousel"></section>
+		<section class="listSliderRow" id="category_carousel">
+			<div class="titleContainer">
+				<h2>shop by category</h2>
+			</div>
+			<div class="contentList">
+				<button
+					class="sliderInteractBtn fa-sharp fa-solid fa-angle-left"
+					style="margin-left: -8px"></button>
+				<div class="listSlide catSlide">
+					<div class="slideEl">
+						<div class="card">
+							<div class="cardThumb">
+								<img
+									src="images/category/category-2.jpg"
+									alt="" />
+							</div>
+						</div>
+						<div class="cardDetails">
+							<h3>Eyes</h3>
+						</div>
+					</div>
+					<div class="slideEl">
+						<div class="card">
+							<div class="cardThumb">
+								<img
+									src="images/category/category-4.jpg"
+									alt="" />
+							</div>
+						</div>
+						<div class="cardDetails">
+							<h3>Face</h3>
+						</div>
+					</div>
+					<div class="slideEl">
+						<div class="card">
+							<div class="cardThumb">
+								<img
+									src="images/category/category-3.jpg"
+									alt="" />
+							</div>
+						</div>
+						<div class="cardDetails">
+							<h3>Lips</h3>
+						</div>
+					</div>
+					<div class="slideEl">
+						<div class="card">
+							<div class="cardThumb">
+								<img
+									src="images/category/category-5.jpg"
+									alt="" />
+							</div>
+						</div>
+						<div class="cardDetails">
+							<h3>Skincare</h3>
+						</div>
+					</div>
+					<div class="slideEl">
+						<div class="card">
+							<div class="cardThumb">
+								<img
+									src="images/category/category-1.jpg"
+									alt="" />
+							</div>
+						</div>
+						<div class="cardDetails">
+							<h3>bestseller</h3>
+						</div>
+					</div>
+				</div>
+				<button
+					class="sliderInteractBtn fa-sharp fa-solid fa-angle-right"
+					style="right: 8px"></button>
+			</div>
+		</section>
 		<!-- Gemini -->
 		<section id="product_showcase"></section>
 		<!-- Ectho -->
-		<section class="carousel" id="top_rating_2"></section>
+		<section class="listSliderRow" id="top_rating_2">
+			<div class="titleContainer centeredTitle">
+				<h2>top rating</h2>
+				<span>Subscribe to be aware of our regular promotions</span>
+			</div>
+			<div class="contentList">
+				<button
+					class="sliderInteractBtn fa-sharp fa-solid fa-angle-left"
+					style="margin-left: -8px"></button>
+				<div class="listSlide">
+					<div class="slideEl">
+						<div
+							class="card"
+							style="
+									--thumb-var-1: url(../images/product/product-30-600x600.jpg);
+									--thumb-var-2: url(../images/product/product-29-600x600.jpg);
+								">
+							<div class="cardTags">
+								<span class="productTag tagHot">hot</span>
+								<span class="productTag tagSale">-17%</span>
+							</div>
+							<div class="cardInteract">
+								<button
+									class="fa-sharp fa-regular fa-heart"></button>
+								<button
+									class="fa-sharp fa-solid fa-magnifying-glass"></button>
+							</div>
+						</div>
+						<div class="cardDetails">
+							<h3><a href="#">the dream mask</a></h3>
+							<span>$50.00 <s>$60.00</s></span>
+						</div>
+					</div>
+					<div class="slideEl">
+						<div
+							class="card"
+							style="
+									--thumb-var-1: url(../images/product/product-48-600x600.jpg);
+									--thumb-var-2: url(../images/product/product-47-600x600.jpg);
+								">
+							<div class="cardInteract">
+								<button
+									class="fa-sharp fa-regular fa-heart"></button>
+								<button
+									class="fa-sharp fa-solid fa-magnifying-glass"></button>
+							</div>
+						</div>
+						<div class="cardDetails">
+							<h3><a href="#">The hydra serum</a></h3>
+							<span>$40.00</span>
+						</div>
+					</div>
+					<div class="slideEl">
+						<div
+							class="card"
+							style="
+									--thumb-var-1: url(../images/product/product-37-600x600.jpg);
+									--thumb-var-2: url(../images/product/product-38-600x600.jpg);
+								">
+							<div class="cardTags">
+								<span class="productTag tagHot">hot</span>
+							</div>
+							<div class="cardInteract">
+								<button
+									class="fa-sharp fa-regular fa-heart"></button>
+								<button
+									class="fa-sharp fa-solid fa-magnifying-glass"></button>
+							</div>
+						</div>
+						<div class="cardDetails">
+							<h3><a href="#">The face jelly</a></h3>
+							<span>$40.00 <s>$56.00</s></span>
+						</div>
+					</div>
+					<div class="slideEl">
+						<div
+							class="card"
+							style="
+									--thumb-var-1: url(../images/product/product-45-600x600.jpg);
+									--thumb-var-2: url(../images/product/product-46-600x600.jpg);
+								">
+							<div class="cardTags">
+								<span class="productTag tagSale">-15%</span>
+							</div>
+							<div class="cardInteract">
+								<button
+									class="fa-sharp fa-regular fa-heart"></button>
+								<button
+									class="fa-sharp fa-solid fa-magnifying-glass"></button>
+							</div>
+						</div>
+						<div class="cardDetails">
+							<h3><a href="#">the glow serum</a></h3>
+							<span>$55.00 <s>$65.00</s></span>
+						</div>
+					</div>
+					<div class="slideEl">
+						<div
+							class="card"
+							style="
+									--thumb-var-1: url(../images/product/360-1-600x600.jpg);
+									--thumb-var-2: url(../images/product/360-2-600x600.jpg);
+								">
+							<div class="cardTags">
+								<span class="productTag tagHot">hot</span>
+							</div>
+							<div class="cardInteract">
+								<button
+									class="fa-sharp fa-regular fa-heart"></button>
+								<button
+									class="fa-sharp fa-solid fa-magnifying-glass"></button>
+							</div>
+						</div>
+						<div class="cardDetails">
+							<h3>
+								<a href="#">daymask peptide lip balm</a>
+							</h3>
+							<span>$50.00</span>
+						</div>
+					</div>
+				</div>
+				<button
+					class="sliderInteractBtn fa-sharp fa-solid fa-angle-right"
+					style="right: 8px"></button>
+			</div>
+		</section>
 		<!-- Ectho -->
-		<section id="products_recommendation"></section>
+		<section id="products_recommendation">
+			<div class="productCard largeCard">
+				<span>makeup & life blending</span>
+				<h2>for flawless skin</h2>
+				<a href="#" class="shopBtn">shop now</a>
+			</div>
+			<div class="productCard">
+				<h2>pure skin gel</h2>
+				<a href="#" class="shopBtn slimBtn">shop now</a>
+			</div>
+		</section>
 	</main>
 	<!-- Gemini -->
 	<?php include 'includes/footer.php' ?>
