17cdc6b01247529819fb9b5a533b323be09d0e4d
Piquixel <thomas.fetu2004@gmail.com>
2025-01-14 15:17:02 +0100

    cache rebuild

---
+++ includes/footer.php
@@ -0,0 +1,70 @@
+<footer>
+    <div>
+        <h2>Contact</h2>
+        <ul>
+            <li>
+                <img src="images/icons/phone.svg" alt="" />
+                <p>Call Phone</p>
+            </li>
+            <li>
+                <a href="#"> (+84) 123 567 712</a>
+            </li>
+            <li>
+                <img src="images/icons/mail.svg" alt="" />
+                <p>Mail Us</p>
+            </li>
+            <li>
+                <a href="#"> Arioshop@Gmail.Com</a>
+            </li>
+        </ul>
+    </div>
+    <div>
+        <h2>About Us</h2>
+        <ul>
+            <li><a href="#">FAQs</a></li>
+            <li><a href="#">Shipping</a></li>
+            <li><a href="#">Returns</a></li>
+            <li><a href="#">Size Guides</a></li>
+            <li><a href="#">Materials & Care</a></li>
+            <li><a href="#">Contact Us</a></li>
+        </ul>
+    </div>
+    <div>
+        <h2>Corporate</h2>
+        <ul>
+            <li><a href="#">Careers</a></li>
+            <li><a href="#">About Us</a></li>
+            <li><a href="#">Code Of Ethics</a></li>
+            <li><a href="#">Event Sponsorships</a></li>
+            <li><a href="#">Vendors</a></li>
+            <li><a href="#">Affiliate Program</a></li>
+        </ul>
+    </div>
+    <div>
+        <h2>Follow Us</h2>
+        <ul>
+            <li><a href="#">Twitter</a></li>
+            <li><a href="#">Facebook</a></li>
+            <li><a href="#">Instagram</a></li>
+            <li><a href="#">Pinterest</a></li>
+        </ul>
+    </div>
+    <div>
+        <div>
+            <p>
+                Subscribe to be aware of our regular promotions &
+                exclusives offers!
+            </p>
+            <div>
+                <input type="email" placeholder="Your email…" />
+                <button
+                    class="fa-solid fa-sharp fa-arrow-right-long"
+                    type="submit"></button>
+            </div>
+        </div>
+        <div>
+            <img src="images/logo.png" width="126px" />
+            <p>Copyright &copy; 2024. All Rights Reserved.</p>
+        </div>
+    </div>
+</footer>
---
+++ includes/header.php
@@ -0,0 +1,69 @@
+<header>
+    <nav id="navbar-desktop">
+        <div id="top-row">
+            <div id="social-panel">
+                <ul>
+                    <li>
+                        <a href="#" class="fa-brands fa-tiktok"></a>
+                    </li>
+                    <li>
+                        <a href="#" class="fa-brands fa-x-twitter"></a>
+                    </li>
+                    <li>
+                        <a href="#" class="fa-brands fa-instagram"></a>
+                    </li>
+                    <li>
+                        <a href="#" class="fa-brands fa-facebook-f"></a>
+                    </li>
+                </ul>
+            </div>
+            <a href="./index.php" id="logo">
+                <img src="./images/logo.png" alt="Logo Ario" />
+            </a>
+            <div id="nav-panel">
+                <span class="fa-solid fa-magnifying-glass"></span>
+                <a href="#" class="fa-regular fa-user"></a>
+                <a href="#" class="fa-sharp fa-regular fa-heart"></a>
+                <a href="#" class="fa-solid fa-bag-shopping"></a>
+            </div>
+        </div>
+        <div id="bottom-row">
+            <ul>
+                <li class="navLink">
+                    <a href="#">Home</a>
+                </li>
+                <li class="navLink">
+                    <a href="#">Shop</a>
+                </li>
+                <li class="navLink">
+                    <a href="#">Products</a>
+                </li>
+                <li class="navLink">
+                    <a href="#">Blog</a>
+                </li>
+                <li class="navLink">
+                    <a href="#">Page</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    <nav id="navbar-mobile">
+        <div>
+            <div id="sliding-container"></div>
+            <div>
+                <a href="index.php">
+                    <img src="./images/logo.png" alt="Logo Ario" />
+                </a>
+            </div>
+            <div>
+                <i class="fa-solid fa-bag-shopping"></i>
+            </div>
+            <div id="navbar-mobile-endpage">
+                <a href="#" class="fa-solid fa-border-all"></a>
+                <a href="#" class="fa-regular fa-user"></a>
+                <span class="fa-solid fa-magnifying-glass"></span>
+                <a href="#" class="fa-sharp fa-regular fa-heart"></a>
+            </div>
+        </div>
+    </nav>
+</header>
